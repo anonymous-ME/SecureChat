e85ca935ea3de29466b14e595e0d93d171066084
Affan <affanahmadfahmi@gmail.com>
2016-11-14 12:58:06 +0530

    Final StartScreen

---
+++ src/data_encryption_system/StartScreen.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data_encryption_system;
+
+/**
+ *
+ * @author affan
+ */
+// StartScreen.java
+// A simple application to show a title screen in the center of the screen
+// for the amount of time given in the constructor.  This class includes
+// a sample main() method to test the splash screen, but it's meant for use
+// with other applications.
+//
+
+import java.awt.*;
+import javax.swing.*;
+
+public class StartScreen extends JWindow {
+  private int duration;
+  public StartScreen(int d) {
+    duration = d;
+  }
+
+  // A simple little method to show a title screen in the center
+  // of the screen for the amount of time given in the constructor
+  public void showSplash() {
+    JPanel content = (JPanel)getContentPane();
+    content.setBackground(Color.white);
+
+    // Set the window's bounds, centering the window
+    int width = 450;
+    int height =280;
+    Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+    int x = (screen.width-width)/2;
+    int y = (screen.height-height)/2;
+    setBounds(x,y,width,height);
+
+    // Build the splash screen
+    JLabel label = new JLabel(new ImageIcon(getClass().getResource("logo.png")));
+    JLabel copyrt = new JLabel
+      ("Copyright 2016, SECURE CHAT", JLabel.CENTER);
+    copyrt.setFont(new Font("Sans-Serif", Font.BOLD, 12));
+    content.add(label, BorderLayout.CENTER);
+    content.add(copyrt, BorderLayout.SOUTH);
+    Color oraRed = new Color(125, 140, 20,  240);
+    content.setBorder(BorderFactory.createLineBorder(oraRed, 10));
+    setAlwaysOnTop(true);
+    // Display it
+    setVisible(true);
+
+    // Wait a little while, maybe while loading resources
+    try { Thread.sleep(duration); } catch (Exception e) {}
+
+    setVisible(false);
+  }
+
+  public void showSplashAndExit() {
+    showSplash();
+    dispose();
+    new main().setVisible(true);
+  }
+
+  public static void main(String[] args) {
+    // Throw a nice little title page up on the screen first
+            try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+    StartScreen splash = new StartScreen(5000);
+    // Normally, we'd call splash.showSplash() and get on with the program.
+    // But, since this is only a test...
+    splash.showSplashAndExit();
+  }
+}
